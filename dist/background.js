9bb8bc03cc5a65428b29d6e712c01e31134f29d2
kongfanbo@yundun.com <xank@qq.com>
2018-09-13 10:45:27 +0800

    add notice

--- dist/background.js
+++ dist/background.js
@@ -1,5 +1,6 @@
 ;(function() {
   var interval
+
   function initProgress() {
     axios.get('http://192.168.5.189:9100/progresses').then(function(response) {
       const { data } = response
@@ -11,6 +12,25 @@
         chrome.browserAction.setBadgeText({ text: '' })
       }
     })
+
+    axios
+      .get('http://192.168.5.189:9100/notices?clientid=yundun')
+      .then(function(response) {
+        const { data = [] } = response
+
+        data.forEach(item => {
+          const { content } = item
+
+          const [name, status] = content.split(',')
+
+          chrome.notifications.create({
+            iconUrl: 'images/yundun-128.png',
+            title: name,
+            type: 'basic',
+            message: status
+          })
+        })
+      })
   }
 
   initProgress()
