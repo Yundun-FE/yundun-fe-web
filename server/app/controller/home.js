bfaa87fc7bf94bcb7ff517e199b1bb2c3d830bdf
kongfanbo@yundun.com <xank@qq.com>
2018-10-09 09:43:11 +0800

    server

--- server/app/controller/home.js
+++ server/app/controller/home.js
@@ -1,10 +1,19 @@
 'use strict';
 
 const Controller = require('egg').Controller;
+const path = require('path');
+const fs = require('fs');
+const file = path.resolve(__dirname, '../../../dist/index.html');
+
+let index;
+fs.readFile(file, (err, data) => {
+  index = data;
+});
 
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = 'yundun-fe-web';
+    this.ctx.set('Content-Type', 'text/html');
+    this.ctx.body = index;
   }
 }
 
--- server/config/config.default.js
+++ server/config/config.default.js
@@ -11,6 +11,7 @@ module.exports = appInfo => {
   config.middleware = [];
 
   config.static = {
+    prefix: '/',
     dir: path.join(appInfo.baseDir, '../dist'),
   };
 
