54e2e8483b635cffb1bc7c2b826d8e2be64fb3c5
xank <xank@qq.com>
2019-01-25 16:21:15 +0800

    account

--- src/pages/product/info.vue
+++ src/pages/product/info.vue
@@ -5,18 +5,14 @@
       v-model="form"
       :rules="rules"
       @submit="handleSubmit"
-    >
-      <FormRow />
-    </DmEdit>
+    />
   </page>
 </template>
 
 <script>
-import FormRow from './components/FormRow'
 import consoleEdit from '@/mixins/consoleEdit'
 
 export default {
-  components: { FormRow },
 
   mixins: [consoleEdit],
 
