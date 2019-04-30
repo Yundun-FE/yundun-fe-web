b4ea911a13829dca819b00962b07a89f43c39995
xank <xank@qq.com>
2019-04-30 17:29:12 +0800

    ui

--- src/pages/Settings/Profile.vue
+++ src/pages/Settings/Profile.vue
@@ -6,20 +6,21 @@
     <a-tabs tab-position="left" default-active-key="info">
       <a-tab-pane key="account" tab="账户信息">账户信息</a-tab-pane>
       <a-tab-pane key="info" tab="个人信息"><TabInfo/></a-tab-pane>
-      <a-tab-pane key="email" tab="邮箱">邮箱</a-tab-pane>
+      <a-tab-pane key="email" tab="邮箱"><TabEmail/></a-tab-pane>
       <a-tab-pane key="organization" tab="我的组织">我的组织</a-tab-pane>
-      <a-tab-pane key="loginSetting" tab="Tab 3">登陆设置</a-tab-pane>
-      <a-tab-pane key="notice" tab="Tab 3">通知</a-tab-pane>
-      <a-tab-pane key="api" tab="Tab 3">API</a-tab-pane>
+      <!-- <a-tab-pane key="loginSetting" tab="登陆设置">登陆设置</a-tab-pane> -->
+      <a-tab-pane key="notice" tab="通知">通知</a-tab-pane>
+      <a-tab-pane key="api" tab="API">API</a-tab-pane>
     </a-tabs>
   </div>
 </template>
 
 <script>
 import TabInfo from './components/TabInfo'
+import TabEmail from './components/TabEmail'
 
 export default {
-  components: { TabInfo },
+  components: { TabInfo, TabEmail },
 
   props: {},
 
---
+++ src/pages/Settings/components/TabEmail.vue
@@ -0,0 +1,34 @@
+<style lang="less">
+</style>
+
+<template>
+  <a-card>
+    <div slot="title">
+      邮箱
+    </div>
+    <p>您可以关联和修改您的邮件地址，并设置其中的一个为主邮箱。您的主邮箱将被用于登陆账户以及接收相关的邮件通知（例如，账户提醒和计费收据)。</p>
+
+    <a-list/>
+
+    <a-form>
+      <a-form-item>
+        <a-input placeholder="输入新邮箱"/>
+        <a-button>添加</a-button>
+      </a-form-item>
+    </a-form>
+  </a-card>
+</template>
+
+<script>
+export default {
+  props: {},
+
+  data() {
+    return {}
+  },
+
+  computed: {},
+
+  methods: {}
+}
+</script>
