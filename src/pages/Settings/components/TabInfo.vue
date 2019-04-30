a63bb879c429e75f026501bd607a1a7f433a97af
xank <xank@qq.com>
2019-04-30 15:37:57 +0800

    info

--- src/pages/Settings/components/TabInfo.vue
+++ src/pages/Settings/components/TabInfo.vue
@@ -9,9 +9,43 @@
         <a-form-item
           :label-col="formItemLayout.labelCol"
           :wrapper-col="formItemLayout.wrapperCol"
-          label="头像">
-          <a-avatar :size="64" icon="user" />
+          label="头像"
+          help="支持 JPG, PNG 格式，文件小于 1M。"
+        >
+          <a-avatar
+            :size="64"
+            :src="userInfo.avatar"
+            icon="user"
+          />
+          <div>
+            <a-upload
+              :headers="authHeaders()"
+              name="file"
+              action="api/v1/user/avatar"
+              @change="handleChange"
+            >
+              <a-button type="primary">
+                <a-icon type="upload" />选择图片
+              </a-button>
+            </a-upload>
+          </div>
           <!--  -->
+        </a-form-item>
+        <a-form-item
+          :label-col="formItemLayout.labelCol"
+          :wrapper-col="formItemLayout.wrapperCol"
+          label="用户名"
+        >
+          <a-input
+            v-model="userInfo.username"
+            style="width: 280px"
+            disabled
+          >
+            <a-icon
+              slot="suffix"
+              type="lock"
+            />
+          </a-input>
         </a-form-item>
         <a-form-item
           :label-col="formItemLayout.labelCol"
@@ -19,7 +53,33 @@
           label="手机号"
         >
           <!--  -->
-          <a-input v-model="userInfo.phoneNumber" />
+          <a-input
+            v-model="userInfo.phoneNumber"
+            style="width: 280px"
+          />
+          <a-button
+            style="margin-left: 12px"
+            type="primary"
+          >修改</a-button>
+        </a-form-item>
+        <a-form-item
+          :label-col="formItemLayout.labelCol"
+          :wrapper-col="formItemLayout.wrapperCol"
+          label="更改密码"
+        >
+          <!--  -->
+          <a-input
+            :value="12345678"
+            disabled
+            type="password"
+            style="width: 280px"
+          >
+            <a-icon
+              slot="suffix"
+              type="lock"
+            />
+          </a-input>
+          <a-button style="margin-left: 12px">更改</a-button>
         </a-form-item>
       </a-form>
 
@@ -29,14 +89,11 @@
 
 <script>
 import userMixins from '@/mixins/user'
+import { authHeaders } from '@/utils/auth'
 
 const formItemLayout = {
   labelCol: { span: 4 },
-  wrapperCol: { span: 8 }
-}
-const formTailLayout = {
-  labelCol: { span: 4 },
-  wrapperCol: { span: 8, offset: 4 }
+  wrapperCol: { span: 20 }
 }
 
 export default {
@@ -46,8 +103,8 @@ export default {
 
   data() {
     return {
-      formItemLayout,
-      formTailLayout
+      authHeaders,
+      formItemLayout
     }
   },
 
